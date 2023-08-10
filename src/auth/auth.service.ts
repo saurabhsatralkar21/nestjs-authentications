d1480a1fd7d49fd3793b99132dfe445a622896b1
Saurabh S <saurabh.satralkar7@gmail.com>
2023-08-10 20:17:10 +0200

    Auth service created

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,6 +6,14 @@ export class AuthService {
     constructor(private userService: UsersService){}
 
     async validaUser(username: string, password: string): Promise<any>{
-        
+        const user = await this.userService.findOne(username);
+
+        if(user && user.password === password) {
+            const {password, username, ...rest} = user;
+            return rest;
+        }
+
+        return null
+
     }
 }
