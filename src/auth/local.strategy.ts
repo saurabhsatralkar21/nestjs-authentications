a66ac2b8cd20d700525e2370d892887c82d32ce6
Saurabh S <saurabh.satralkar7@gmail.com>
2023-08-10 20:27:24 +0200

    Auth module complete

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { Strategy } from "passport-local";
 import { AuthService } from "./auth.service";
@@ -6,6 +6,16 @@ import { AuthService } from "./auth.service";
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy){
     constructor(private authServe: AuthService) {
-        super(); // config
+        super(); // config, when the authentication strategy will be from 3rd party provider
+    }
+
+    async validate(username:string, password: string): Promise<any>{
+        const user = await this.authServe.validaUser(username, password);
+
+        if(!user){
+            throw new UnauthorizedException();
+        }
+
+        return user;
     }
 }
